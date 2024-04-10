ada82106e2e8fb29d1710df482e3aa6253072587
Sombody101 <evansheyborne@gmail.com>
2024-04-10 17:48:41 -0600

    Help information update

--- src/argparse/argparse.go
+++ src/argparse/argparse.go
@@ -12,15 +12,15 @@ import (
 type options struct {
 
 	/* Positional */
-	SearchString     string `arg:"positional" help:"The string FindIt will look for in files"`
+	SearchString     string `arg:"positional" help:"The string FindIt will look for in files within a directory"`
 	WorkingDirectory string `arg:"positional" default:"." help:"The directory for FindIt to look in"`
 
 	/* Switches */
 	FileType         string `arg:"-t:--type" default:"*" help:"Specify a file type (or extension)"`
 	ContextSize      int    `arg:"-c,--context-size" default:"20" help:"How many characters should be appended to the found strings context"`
 	ColorMode        string `arg:"--color" default:"auto" help:"Specify if output should be colored (never, always, auto)"`
-	AllowBinaryFiles bool   `arg:"-b,--allow-binary" default:"false" help:"Scan files detected as binary for the given search string"`
-	OnlyBinaryFiles  bool   `arg:"--only-binary" default:"false" help:"Only scan files detected as binary for the given search string"`
+	AllowBinaryFiles bool   `arg:"-b,--allow-binary" default:"false" help:"Scan files detected as binary for the given search string (Not recommended)"`
+	OnlyBinaryFiles  bool   `arg:"--only-binary" default:"false" help:"Only scan files detected as binary for the given search string (Not recommended)"`
 
 	/* Debugging */
 	Verbose bool `arg:"--verbose" default:"false"`
